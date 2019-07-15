79d05d75401b427005b29d704e1366c8e39ab627
Takayuki Shimada <taka@tsmd.jp>
2019-07-15 15:43:38 +0900

    refactor: 型付け

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,10 @@
-/** nCr */
-export function combination(n, r, offset = 0) {
-  const combs = []
-  for (var i = 0; i <= n - r; i += 1) {
+/**
+ * 組み合わせ nCr を求めて２次元配列を取得する。
+ * 例えば 3C2 の結果は [[0, 1], [0, 2], [1, 2]]
+ */
+export function combination(n: number, r: number, offset = 0): number[][] {
+  const combs: number[][] = []
+  for (let i = 0; i <= n - r; i += 1) {
     if (r === 1) {
       combs.push([offset + i])
     } else {
