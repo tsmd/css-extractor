cbb3dac62e89f796522bd8926856d40fda1a99b9
Takayuki Shimada <taka@tsmd.jp>
2019-07-15 22:11:19 +0900

    feat: add UI for options

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,6 +2,16 @@ import $ from 'jquery'
 import { extract } from 'css-scaffolder'
 
 $('#button').on('click', function() {
-  const css = extract($('#html').val())
+  const options = { reset: reset }
+
+  options.reset = $('#reset').prop('checked')
+
+  const ignore = $('#ignore').val()
+  if (ignore) {
+    options.ignorePatternForSingleClass = new RegExp(ignore)
+    console.log(options.ignorePatternForSingleClass)
+  }
+
+  const css = extract($('#html').val(), options)
   $('#css').val(css)
 })
